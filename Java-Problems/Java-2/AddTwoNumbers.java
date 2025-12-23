a2d000d8e946656ab7a8808084805ff20ef85e0c
codeKrantz <conradkrantz@gmail.com>
2025-12-23 07:17:59 -0600

    added better solution, runtime, and how it works

--- Java-Problems/Java-2/AddTwoNumbers.java
+++ Java-Problems/Java-2/AddTwoNumbers.java
@@ -8,28 +8,36 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+ /**
+  * QUESTION NUMBER: 2
+  *  RUNTIME:O(n)
+  HOW IT WORKS: (throws errors because ListNode is given in the problem), Iterates through each digit and adds it 
+  to a dummy link list. A carry is used to ensure digits are mathmatically correct before adding and interating
+  to the next node. 
+ */
 class AddTwoNumbers{
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
-        ListNode tail = dummy;
+        ListNode dummy = new ListNode(0);// a dummy node that we add to
+        ListNode current = dummy;
+        int carry = 0; // like in"carry the one"
 
-        int carry = 0;
-
-        while (l1 != null || l2 != null || carry != 0) {
-            int x = (l1 != null) ? l1.val : 0;
-            int y = (l2 != null) ? l2.val : 0;
-
-            int sum = x + y + carry;
-            carry = sum / 10;
-            int digit = sum % 10;
-
-            tail.next = new ListNode(digit);
-            tail = tail.next;
-
-            if (l1 != null) l1 = l1.next;
-            if (l2 != null) l2 = l2.next;
+        while(l1 != null || l2 != null || carry == 1){
+            int sum = 0;
+            if(l1 != null){
+                sum += l1.val;//get the value
+                l1 = l1.next;//advance to next node
+            }
+            if(l2 != null){
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            sum += carry;
+            carry = sum/10; //needs to be a single digit
+            ListNode node = new ListNode(sum %10); // add a single node to the list
+            current.next = node;
+            current = current.next;
         }
-
-        return dummy.next;
+        return dummy.next;// need the next node
     }
 }
