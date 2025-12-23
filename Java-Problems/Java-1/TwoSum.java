a4f9373770efb02bdc9c7e47b0cd2262d7b08610
codeKrantz <conradkrantz@gmail.com>
2025-12-23 06:10:24 -0600

    Run time and how it works

--- Java-Problems/Java-1/TwoSum.java
+++ Java-Problems/Java-1/TwoSum.java
@@ -1,7 +1,12 @@
 
 
 
-
+/**
+ * RUN TIME: O(n^2)
+ * HOW IT WORKS: A double for loop iterates one ahead and behind to search the whole
+ * list to find the two numbers that equal the target
+ * 
+*/
 class TwoSum{
      public int[] twoSum(int[] nums, int target) {
         int[] answers = new int[2];
