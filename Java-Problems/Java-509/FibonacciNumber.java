96fd015bf985b5fa617213f0a943e946088485e1
codeKrantz <conradkrantz@gmail.com>
2026-01-06 16:46:58 -0600

    added solution

---
+++ Java-Problems/Java-509/FibonacciNumber.java
@@ -0,0 +1,11 @@
+public class FibonacciNumber {
+    public int fib(int n) {
+        //base case
+        if(n == 0 || n == 1){
+            return n;
+        }
+        //recusive call
+        return (fib(n-1) + fib(n-2));
+
+    }
+}
