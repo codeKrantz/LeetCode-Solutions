0d8d0b57462c6bef6e5fb215d1cef3fc7d90dab9
codeKrantz <conradkrantz@gmail.com>
2026-01-10 11:05:45 -0600

    added solution

---
+++ Java-Problems/Java-110/BalancedBinaryTree.java
@@ -0,0 +1,40 @@
+public class BalancedBinaryTree {
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+    public boolean isBalanced(TreeNode root) {
+        return heightDiff(root) != -1;
+    }
+
+    public int heightDiff(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+
+        int leftHeight = heightDiff(root.left);
+        if(leftHeight == -1){
+            return -1;
+        }
+        int rightHeight = heightDiff(root.right);
+        if(rightHeight == -1){
+            return -1;
+        }
+
+        if(Math.abs(leftHeight -rightHeight) > 1){
+            return -1;
+        }
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+}
